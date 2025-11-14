602b4f2c2c1f6b92f6f5197e9aae58e71110bc5e
노미경 <codingworld@naver.com>
2025-11-14 15:10:37 +0900

    convert: Tag.java -> .kt

--- src/main/java/com/back/pinco/domain/tag/entity/Tag.kt
+++ src/main/java/com/back/pinco/domain/tag/entity/Tag.kt
@@ -1,45 +1,28 @@
-package com.back.pinco.domain.tag.entity;
+package com.back.pinco.domain.tag.entity
 
-import com.back.pinco.global.jpa.entity.BaseEntity;
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import com.back.pinco.global.jpa.entity.BaseEntity
+import jakarta.persistence.*
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 
 @Entity
-@NoArgsConstructor
-@Getter
 @Table(
-        name = "tags",
-        indexes = {
-                @Index(name = "idx_tag_keyword", columnList = "keyword")
-        }
+    name = "tags",
+    indexes = [Index(name = "idx_tag_keyword", columnList = "keyword")]
 )
-@EntityListeners(AuditingEntityListener.class)
+@EntityListeners(AuditingEntityListener::class)
 @SequenceGenerator(
-        name = "tag_id_gen",
-        sequenceName = "TAG_SEQ",
-        initialValue = 1,
-        allocationSize = 50
+    name = "tag_id_gen",
+    sequenceName = "TAG_SEQ",
+    initialValue = 1, allocationSize = 50
 )
-public class Tag extends BaseEntity {
-
+class Tag(
+    @Column(name = "keyword", nullable = false, unique = true, length = 50)
+    var keyword: String
+) : BaseEntity() {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tag_id_gen")
     @Column(name = "tag_id")
-    private Long id;    // 고유 ID
-
-    @Column(name = "keyword", nullable = false, unique = true, length = 50)
-    private String keyword;    // 키워드
-
-
-    public Tag(String keyword) {
-        this.keyword = keyword;
-    }
-    
-    public Tag(Long id, String keyword) { //태그 생성용
-        this.id = id;
-        this.keyword = keyword;
-    }
+    var id: Long? = null // 고유 ID
+        protected set
 
 }
