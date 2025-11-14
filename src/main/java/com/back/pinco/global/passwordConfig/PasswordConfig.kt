d97dddbefa99f57854f1149ea59ad5bed1f17b63
Jung Yunseo <yunseo2488@naver.com>
2025-11-14 11:25:52 +0900

    refactor : passwordConfig 코틀린 전환

--- src/main/java/com/back/pinco/global/passwordConfig/PasswordConfig.kt
+++ src/main/java/com/back/pinco/global/passwordConfig/PasswordConfig.kt
@@ -1,15 +1,12 @@
-package com.back.pinco.global.passwordConfig;
+package com.back.pinco.global.passwordConfig
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
-public class PasswordConfig {
-
+class PasswordConfig {
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 }
