62cb45021e7c2f6eb3163f5c603e0db49f4cc8c5
노미경 <codingworld@naver.com>
2025-11-14 09:40:48 +0900

    refactor: convert SwaggerConfig.java -> .kt

--- src/main/java/com/back/pinco/global/swaggerConfig/SwaggerConfig.kt
+++ src/main/java/com/back/pinco/global/swaggerConfig/SwaggerConfig.kt
@@ -1,18 +1,17 @@
-package com.back.pinco.global.swaggerConfig;
+package com.back.pinco.global.swaggerConfig
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 @Configuration
-public class SwaggerConfig {
-
+class SwaggerConfig {
     @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("PINCO API")
-                        .description("위치 기반 장소 공유 서비스"));
-    }
+    fun openAPI(): OpenAPI =
+        OpenAPI().info(
+            Info()
+                .title("PINCO API")
+                .description("위치 기반 장소 공유 서비스")
+        )
 }
